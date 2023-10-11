3ab144e950b5be846a19905f6f32d764e1fc978b
pragatibirajdar-12 <139330167+pragatibirajdar-12@users.noreply.github.com>
2023-10-11 22:19:17 +0530

    Add files via upload
    SpringBoot Demo app

---
+++ SpringBoootFirstApp/backend/src/main/java/com/example/backend/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.backend.service;
+
+import java.util.List;
+
+import com.example.backend.model.Employee;
+
+public interface EmployeeService {
+
+	Employee saveEmployee(Employee employee);
+	List<Employee> getAllEmployees();
+	Employee getEmployeeById(long id);
+	Employee updateEmployee(Employee employee, long id);
+	void deleteEmployee(long id);
+}
